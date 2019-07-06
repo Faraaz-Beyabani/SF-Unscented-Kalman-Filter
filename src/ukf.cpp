1942e99d14a42eb57be5a8cb8a69efaefd3f59c9
Faraaz <beyabanifaraaz@gmail.com>
2019-07-06 04:12:19 +0000

    Finished initialization function with basic parameters. Must implement Prediction and UpdateL/R functions

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -21,10 +21,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.5;
   
   /**
    * DO NOT MODIFY measurement noise values below.
@@ -55,6 +55,18 @@ UKF::UKF() {
    * Hint: one or more values initialized above might be wildly off...
    */
   is_initialized_ = false;
+  
+  n_x_ = 5;
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_;
+  
+  weights_ = VectorXd(2*n_aug_+1);
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_+1);
+  
+  weights_(0) = lambda_/(lambda_+n_aug_);
+  for(int i = 1; i < weights_.size(); i++) {
+    weights_(i) = 1/2*(lambda_+n_aug_);
+  }
 }
 
 UKF::~UKF() {}
@@ -64,6 +76,57 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
    * TODO: Complete this function! Make sure you switch between lidar and radar
    * measurements.
    */
+  if(!is_initialized_) {
+    is_initialized_ = true;
+   	P_.setIdentity(n_x_, n_x_);
+    
+    VectorXd raw_vals = meas_package.raw_measurements_;
+    
+    double px, py;
+    
+    switch(meas_package.sensor_type_) {
+      case MeasurementPackage::LASER:
+        px = raw_vals[0];
+        py = raw_vals[1];
+
+        x_ << px, py, 0, 0, 0;
+        break;
+      case MeasurementPackage::RADAR:
+        double rho, phi, rhod;
+        rho = raw_vals[0];
+        phi = raw_vals[1];
+
+        px = rho*cos(phi);
+        py = rho*sin(phi);
+
+        x_ << px, py, 0, 0, 0;
+        break;
+      default:
+        return;
+    }
+    
+    time_us_ = meas_package.timestamp_;
+    
+    return;
+  }
+  
+  double delta_t = meas_package.timestamp_ - time_us_;
+  Prediction(delta_t);
+  
+  switch(meas_package.sensor_type_) {
+    case MeasurementPackage::LASER:
+      UpdateLidar(meas_package);
+      break;
+    case MeasurementPackage::RADAR:
+      UpdateRadar(meas_package);
+      break;
+    default:
+      return;
+  }
+  
+  time_us_ = meas_package.timestamp_;
+  
+  return;
 }
 
 void UKF::Prediction(double delta_t) {
